91a2a09530f2e33c3946483acc52024842c5074d
Koen Mulder <Noek1993@outlook.com>
2014-12-08 14:39:41 +0100

    FS-28 First version of the  virtual input and output stream

---
+++ src/main/java/org/fides/components/virtualstream/VirtualInputStream.java
@@ -0,0 +1,74 @@
+package org.fides.components.virtualstream;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * A virtual {@link InputStream} that simulates an {@link InputStream} on top of an existing {@link InputStream}. This
+ * stream does not close the underlying {@link InputStream}.
+ */
+public class VirtualInputStream extends FilterInputStream {
+
+	private short bytesLeft = 0;
+
+	private InputStream in;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param in
+	 */
+	protected VirtualInputStream(InputStream in) {
+		super(in);
+	}
+
+	@Override
+	public int read() throws IOException {
+		while (bytesLeft == 0) {
+			readPrefix();
+		}
+		if (bytesLeft < 0) {
+			return -1;
+		}
+		--bytesLeft;
+		return in.read();
+	}
+
+	private void readPrefix() throws IOException {
+		// Read the prefix
+		byte[] prefix = new byte[2];
+		int read = in.read(prefix);
+		if (read != 2) {
+			throw new IOException("Prefix does not have the right size");
+		}
+		bytesLeft = ByteBuffer.wrap(prefix).getShort();
+	}
+
+	@Override
+	public void close() throws IOException {
+		// We do not want to close the underlying InputStream
+	}
+
+	@Override
+	public long skip(long n) throws IOException {
+		throw new UnsupportedOperationException("skip is not supported");
+	}
+
+	@Override
+	public synchronized void mark(int readlimit) {
+		throw new UnsupportedOperationException("mark is not supported");
+	}
+
+	@Override
+	public synchronized void reset() throws IOException {
+		throw new UnsupportedOperationException("reset is not supported");
+	}
+
+	@Override
+	public boolean markSupported() {
+		return false;
+	}
+
+}
---
+++ src/main/java/org/fides/components/virtualstream/VirtualOutputStream.java
@@ -0,0 +1,78 @@
+package org.fides.components.virtualstream;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * A virtual {@link OutputStream} that simulates an {@link OutputStream} on top of an existing {@link OutputStream}.
+ * This is done using a buffer and a custom protocol for writing data. This stream does not close the underlying
+ * {@link OutputStream}.
+ */
+public class VirtualOutputStream extends FilterOutputStream {
+
+	private static final short DEFAULT_BUFFER_SIZE = 1024;
+
+	private final byte[] buffer;
+
+	private short count = 0;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param out
+	 *            The {@link OutputStream} to build the virtual {@link OutputStream} upon
+	 * @param bufferSize
+	 *            The size of the buffer
+	 */
+	public VirtualOutputStream(OutputStream out, short bufferSize) {
+		super(out);
+		buffer = new byte[bufferSize];
+	}
+
+	/**
+	 * Constructor with the default buffer size
+	 * 
+	 * @param out
+	 *            The {@link OutputStream} to build the virtual {@link OutputStream} upon
+	 */
+	public VirtualOutputStream(OutputStream out) {
+		this(out, DEFAULT_BUFFER_SIZE);
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		if (count >= buffer.length) {
+			flushBuffer();
+		}
+		buffer[count++] = (byte) b;
+	}
+
+	/**
+	 * Writes the buffer to the underlying {@link OutputStream} using the virtual stream protocol
+	 * 
+	 * @throws IOException
+	 */
+	private void flushBuffer() throws IOException {
+		byte[] prefix = ByteBuffer.allocate(2).putShort(count).array();
+		out.write(prefix);
+		out.write(buffer, 0, count - 1);
+		count = 0;
+	}
+
+	@Override
+	public void flush() throws IOException {
+		flushBuffer();
+		out.flush();
+	}
+
+	@Override
+	public void close() throws IOException {
+		flushBuffer();
+		byte[] postfix = ByteBuffer.allocate(2).putShort((short) -1).array();
+		out.write(postfix);
+		out.flush();
+	}
+
+}
